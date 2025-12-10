c4ffcc0ca58b1b7371fd6a4141b6ea7d27c2224d
Griffin <80220535+Glloyd5@users.noreply.github.com>
2025-12-09 22:04:50 -0700

    Add new product lineup image

--- wp-content/themes/fmlk-custom-theme/resources/views/front-page.blade.php
+++ wp-content/themes/fmlk-custom-theme/resources/views/front-page.blade.php
@@ -3,11 +3,38 @@
 @section('content')
     <section class="pt-5 my-20">
         <h1 class="text-5xl font-bold text-primary">Award winning hot sauces and catering</h1>
-        <p class="mt-4 text-3xl font-accent text-primary">Never trust a skinny cook</p>
-        <a href="{{ get_permalink(get_page_by_path('shop')) }}"
+    </section>
+
+    <section class="relative w-full my-5">
+        <img src="{{ get_stylesheet_directory_uri() }}/resources/images/ProductLineup.webp"
+            alt="Fatmanlittlekitchen catering brisket dish" class="rounded-lg shadow-lg w-full h-150 object-cover">
+            <a href="{{ get_permalink(get_page_by_path('shop')) }}"
             class="mt-6 inline-block bg-red-600 text-white px-6 py-3 rounded-lg hover:bg-red-700">Our products</a>
         <a href="{{ get_permalink(get_page_by_path('catering')) }}"
             class="mt-6 inline-block bg-red-600 text-white px-6 py-3 rounded-lg hover:bg-red-700">Catering</a>
+    </section>
+
+    <section class="my-20 flex flex-col gap-5">
+        <h2 class="text-2xl">What we do</h3>
+            <p>
+                At Fat Man Little Kitchen, we provide food services, and the thing which started it all; high end
+                sauces and spice blends that are well above industry standard. We also have a business model
+                focused on training, development, ownership potential and the utilization of talented, educated
+                and motivated Indigenous people.
+            </p>
+            <p>
+                We have perfected a 100-year-old recipe from Jamaica and enhanced it with a modern and
+                healthy twist. All our spices use Potassium Chloride instead of table salt to help mitigate the
+                high consumption of Sodium Chloride in modern diets. This is especially true for Indigenous
+                populations where high potassium traditional diets have been replaced by highly processed and
+                ready to eat foods.
+            </p>
+            <p>
+                We are an Indigenous owned company who want to make the world a little bit better of a place. The
+                corporate culture of support, guidance and mentorship for anyone who works for us, will develop new
+                business opportunities, meaningful employment, and training in Indigenous communities, all while
+                taking people to the next level of spicy and exciting food.
+            </p>
     </section>
 
     <section class="relative w-full pb-[56.25%] my-5">
@@ -17,29 +44,6 @@
             allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
             referrerpolicy="strict-origin-when-cross-origin" allowfullscreen>
         </iframe>
-    </section>
-
-    <section class="my-20 flex flex-col gap-5">
-        <h2 class="text-2xl">What we do</h3>
-        <p> 
-            At Fat Man Little Kitchen, we provide food services, and the thing which started it all; high end
-            sauces and spice blends that are well above industry standard. We also have a business model
-            focused on training, development, ownership potential and the utilization of talented, educated
-            and motivated Indigenous people.
-        </p>
-        <p>
-            We have perfected a 100-year-old recipe from Jamaica and enhanced it with a modern and
-            healthy twist. All our spices use Potassium Chloride instead of table salt to help mitigate the
-            high consumption of Sodium Chloride in modern diets. This is especially true for Indigenous
-            populations where high potassium traditional diets have been replaced by highly processed and
-            ready to eat foods.
-        </p>
-        <p>
-            We are an Indigenous owned company who want to make the world a little bit better of a place. The
-            corporate culture of support, guidance and mentorship for anyone who works for us, will develop new
-            business opportunities, meaningful employment, and training in Indigenous communities, all while
-            taking people to the next level of spicy and exciting food.
-        </p>
     </section>
 
     <section id="default-carousel" class="relative w-full py-10" data-carousel="slide">
@@ -77,8 +81,8 @@
             class="absolute top-0 start-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none"
             data-carousel-prev>
             <span class="inline-flex items-center justify-center w-10 h-10 
-                rounded-full group-hover:bg-black/30
-                group-focus:ring-4 group-focus:ring-white">
+                    rounded-full group-hover:bg-black/30
+                    group-focus:ring-4 group-focus:ring-white">
                 <svg class="w-4 h-4 text-gray rtl:rotate-180" xmlns="http://www.w3.org/2000/svg" fill="none"
                     viewBox="0 0 6 10">
                     <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
@@ -93,8 +97,8 @@
             class="absolute top-0 end-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none"
             data-carousel-next>
             <span class="inline-flex items-center justify-center w-10 h-10 
-                rounded-full bg-white/30 group-hover:bg-black/30
-                group-focus:ring-4 group-focus:ring-white">
+                    rounded-full bg-white/30 group-hover:bg-black/30
+                    group-focus:ring-4 group-focus:ring-white">
                 <svg class="w-4 h-4 text-gray rtl:rotate-180" xmlns="http://www.w3.org/2000/svg" fill="none"
                     viewBox="0 0 6 10">
                     <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
@@ -107,7 +111,8 @@
 
     <section class="my-20 flex flex-col gap-5">
         <h3 class="text-2xl">Want to learn more?</h3>
-        <p>Check out our <a class="text-red-700" href="about">about</a> us page or take a look at our products in our <a class="text-red-700" href="shop">shop</a>.</p>
+        <p>Check out our <a class="text-red-700" href="about">about</a> us page or take a look at our products in our <a
+                class="text-red-700" href="shop">shop</a>.</p>
 
     </section>
 @endsection
